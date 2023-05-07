2eca6fdc073850ca8cff71c6a332bcbad6be3a47
Alex Kevakian <akeva001@ucr.edu>
2023-05-06 20:30:09 -0700

    ChatGPT API integration

---
+++ app/api/getWeatherSummary/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import openai from "@/openai";
+
+export async function POST(request: Request) {
+  // weatherdata in the body of the POST req
+  const { weatherData } = await request.json();
+
+  console.log("WEATHERDATA");
+  console.log(JSON.stringify(weatherData));
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: "system",
+        content: `Pretend you're a weather news reporter, reporting LIVE on TV. Be energetic and full of charisma. Introduce yourself as Alex say you are LIVE from the Kevakian Headquarters. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for thos weather conditions such as bringing an umbrella if it is raining etc. Use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data cam from your team at the news office and not the user.`,
+      },
+      {
+        role: "user",
+        content: `Hi there, can i get a summary of todays weather, use the following information to get the weather
+        data: ${JSON.stringify(weatherData)}`,
+      },
+    ],
+  });
+  const { data } = response;
+
+  console.log("DATA IS: ", data);
+
+  return NextResponse.json(data.choices[0].message);
+}
--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -6,6 +6,8 @@ import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
 import TempChart from "@/components/TempChart";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import cleanData from "@/lib/cleanData";
+import getBasePath from "@/lib/getBasePath";
 import React from "react";
 
 export const revalidate = 60;
@@ -35,6 +37,22 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   const results: Root = data.myQuery;
 
+  const dataToSend = cleanData(results, city);
+
+  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      weatherData: dataToSend,
+    }),
+  });
+
+  const GPTdata = await res.json();
+
+  const { content } = GPTdata;
+
   return (
     <div className="flex flex-col min-h-screen md:flex-row md:h-screen">
       <InformationPanel city={city} results={results} long={long} lat={lat} />
@@ -50,7 +68,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
           </div>
 
           <div className="m-2 mb-10">
-            <CalloutCard message="This is where GPT 3.5 summary will go" />
+            <CalloutCard message={content} />
           </div>
 
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
